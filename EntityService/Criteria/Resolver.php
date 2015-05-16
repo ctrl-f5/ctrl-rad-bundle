f7ef26afc5915e302cd27dfd6b7c76fc68aeac7d
Nicky De Maeyer <nicky.demaeyer@gmail.com>
2015-05-16 19:20:38 +0200

    refactored criteria resolver

--- EntityService/Criteria/Resolver.php
+++ EntityService/Criteria/Resolver.php
@@ -40,30 +40,13 @@ class Resolver
      * @param string $rootAlias
      * @return $this
      */
-    public function resolveCriteria(QueryBuilder $queryBuilder, array $criteria = array(), $rootAlias = null)
+    public function resolveCriteria(QueryBuilder $queryBuilder, array $criteria = array())
     {
-        if (!$rootAlias) $rootAlias = $this->getRootAlias();
+        $criteria = $this->unpack($criteria);
 
-        foreach ($criteria as $key => $value) {
-
-            if (is_numeric($key)) {
-                $fieldConfig = $this->getFieldConfig($value, $rootAlias);
-                if ($fieldConfig['is_property']) {
-                    $queryBuilder->andWhere($value);
-                }
-            } else {
-                $fieldConfig = $this->getFieldConfig($key, $rootAlias);
-                $fieldConfig['is_property'] = true;
-
-                $paramId = count($queryBuilder->getParameters()) + 1;
-                $field = $fieldConfig['field'] . ' ' . $fieldConfig['comparison'] . ' ?' . $paramId;
-
-                $queryBuilder->andWhere($field)->setParameter($paramId, $value);
-            }
-            $path = $fieldConfig['path'];
-            if ($fieldConfig['is_property']) array_pop($path);
-            $this->addJoin($queryBuilder, $path, $rootAlias);
-        }
+        foreach ($criteria['joins']         as $join => $alias)     $queryBuilder->join($join, $alias);
+        foreach ($criteria['conditions']    as $where)              $queryBuilder->andWhere($where);
+        foreach ($criteria['parameters']    as $key => $value)      $queryBuilder->setParameter($key, $value);
 
         return $this;
     }
@@ -75,63 +58,134 @@ class Resolver
      */
     public function resolveOrderBy(QueryBuilder $queryBuilder, array $orderBy = array())
     {
-        foreach ($orderBy as $sortField => $order) {
-            $queryBuilder->addOrderBy($sortField, $order);
-        }
+        foreach ($orderBy as $sortField => $order) $queryBuilder->addOrderBy($sortField, $order);
 
         return $this;
     }
 
     /**
-     * @param QueryBuilder $queryBuilder
-     * @param string|array $fieldOrPath
-     * @param string $parentAlias
+     * @param array|string $criteria
+     * @return array
      */
-    protected function addJoin(QueryBuilder $queryBuilder, $fieldOrPath, $parentAlias)
+    public function unpack($criteria)
     {
-        $p = $fieldOrPath;
-        if (is_string($fieldOrPath)) {
-            $config = $this->getFieldConfig($fieldOrPath, $parentAlias);
-            $p = $config['path'];
-        }
-        $joinPart = $queryBuilder->getDQLPart('join');
-        for ($i = 0; $i < count($p); $i++) {
-            foreach ($joinPart as $root => $joins) {
-                foreach ($joins as $join) {
-                    if ($join->getAlias() == $p[$i]) return;
-                }
-            }
-            if ($parentAlias == $p[$i]) continue;
-            $queryBuilder->join($parentAlias . '.' . $p[$i], $p[$i]);
+        $joins = array();
+        $conditions = array();
+        $parameters = array();
+        $paramCount = 0;
 
-            $parentAlias = $p[$i];
+        foreach ((array)$criteria as $key => $val) {
+            $hasValue = is_string($key);
+            $fieldConfig = $hasValue ? $key: $val;
+            $val = (array)$val;
+
+            $expressions = $this->unpackFieldExpression($fieldConfig, $this->getRootAlias());
+            foreach ($expressions as $exr) {
+                $config = $this->getFieldConfig($exr, $hasValue, $paramCount);
+                $path = $config['path'];
+
+                // add conditions and parameters
+                if ($config['is_property']) {
+                    $conditions[] = $config['field'];
+                    if ($config['requires_value']) {
+                        if ($config['has_named_param']) {
+                            $parameters[$config['param_name']] = count($val) == 1 ? array_shift($val): $val[$config['param_name']];
+                        } else {
+                            $parameters[] = array_shift($val);
+                        }
+                    }
+                    array_pop($path);
+                }
+
+                // add joins
+                $joins[] = $path;
+            }
         }
+
+        $joins = $this->mergeJoinPaths($joins);
+
+        return array(
+            'joins' => $joins,
+            'conditions' => $conditions,
+            'parameters' => $parameters,
+        );
+    }
+
+    protected function mergeJoinPaths($joins)
+    {
+        $merged = array();
+        foreach ($joins as $path) {
+            $previous = $path[0];
+            for ($i = 1; $i < count($path); $i++) {
+                $current = $path[$i];
+                $merged[$previous . '.' . $current] = $current;
+                $previous = $current;
+            }
+        }
+
+        return $merged;
+    }
+
+    protected function unpackFieldExpression($expr, $rootAlias)
+    {
+        $result = array();
+        $expr = str_replace(
+            array(' AND ', ' OR '),
+            array(' and ', ' or '),
+            trim(trim($expr), '()')
+        );
+
+        $parts = explode(' and ', $expr);
+        if (count($parts) > 1) {
+            foreach ($parts as $part) $result[] = $this->unpackFieldExpression(trim($part), $rootAlias)[0];
+            return $result;
+        }
+
+        for ($i = 0; $i < count($parts); $i++) {
+            if (strpos($parts[$i], $rootAlias) !== 0) $parts[$i] = $rootAlias . '.' . $parts[$i];
+        }
+
+        return $parts;
     }
 
     /**
      * @param string $expr
-     * @param string $parentAlias
+     * @param bool $hasValue
+     * @param int &$paramCount
      * @return array
      */
-    protected function getFieldConfig($expr, $parentAlias)
+    protected function getFieldConfig($expr, $hasValue, &$paramCount = 0)
     {
-        $parts = explode(' ', $expr);
-        $fieldPart = trim($parts[0], " ()");
+        $parts = explode(' ', trim($expr, " ()"));
+        $fieldPart = array_shift($parts);
         $path = explode('.', $fieldPart);
+        $parent = $path[count($path) - 2];
+        $alias = end($path);
+        $isProp = $hasValue || count($parts) > 1;
 
-        if (count($path) > 1) {
-            $parent = $path[count($path) - 2];
+        $hasNamedParam = strpos($expr, ' :') !== false;
+        $paramName = $hasNamedParam ? trim(end($parts), ':'): null;
+
+        $comp = '=';
+        $requiresValue = true;
+        if (count($parts)) {
+            $condition = implode(' ', $parts);
+            $requiresValue = $hasNamedParam;
         } else {
-            $parent = $parentAlias;
+            $condition = $comp . ' ?' . $paramCount;
+            $paramCount++;
         }
 
         return array(
-            'is_property' => count($parts) > 1,
-            'comparison' => count($parts) > 1 ? $parts[1]: '=',
-            'alias' => end($path),
+            'is_property' => $isProp,
+            'comparison' => $comp,
+            'alias' => $alias,
             'parent' => $parent,
             'path' => $path,
-            'field' => $parent . '.' . end($path),
+            'requires_value' => $requiresValue,
+            'has_named_param' => $hasNamedParam,
+            'param_name' => $paramName,
+            'field' => $parent . '.' . $alias . ' ' . $condition,
         );
     }
 }
--- Test/EntityService/Criteria/ResolverTest.php
+++ Test/EntityService/Criteria/ResolverTest.php
@@ -30,77 +30,245 @@ class ResolverTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('newAlias', $resolver->getRootAlias());
     }
 
-    public function test_resolve_criteria_adds_join()
+    public function test_unpack_conditions()
     {
-        $resolver = $this->getMockBuilder('Ctrl\\RadBundle\\EntityService\\Criteria\\Resolver')
-            ->setConstructorArgs(array('root'))
-            ->setMethods(null)
-            ->getMock()
-        ;
+        $resolver = $this->getResolver("root");
 
-        $qb = $this->getMockBuilder('Doctrine\\ORM\\QueryBuilder')
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
+        $result = $resolver->unpack('id = 1');
+        $this->assertEquals(array(
+            'root.id = 1',
+        ), $result['conditions']);
 
+        $result = $resolver->unpack('id = 1 and id IS NULL');
+        $this->assertEquals(array(
+            'root.id = 1',
+            'root.id IS NULL'
+        ), $result['conditions']);
 
-        $qb->expects($this->once())->method('getDQLPart')->will($this->returnValue(array()));
-        $qb->expects($this->once())->method('join')->with(
-            $this->equalTo('root.test'),
-            $this->equalTo('test')
-        );
+        $result = $resolver->unpack(array('id = 1', 'id IS NULL'));
+        $this->assertEquals(array(
+            'root.id = 1',
+            'root.id IS NULL'
+        ), $result['conditions']);
 
-        $this->assertSame($resolver, $resolver->resolveCriteria($qb, array('test')));
+        $result = $resolver->unpack('root.id = 1 and root.id IS NULL');
+        $this->assertEquals(array(
+            'root.id = 1',
+            'root.id IS NULL'
+        ), $result['conditions']);
+
+        $result = $resolver->unpack('root.id = 1 and (id IS NULL or root.active = false)');
+        $this->assertEquals(array(
+            'root.id = 1',
+            'root.id IS NULL or root.active = false'
+        ), $result['conditions']);
     }
 
-    public function test_resolve_criteria_adds_join_recursively()
+    public function test_unpack_conditions_with_parameters()
     {
-        $resolver = $this->getMockBuilder('Ctrl\\RadBundle\\EntityService\\Criteria\\Resolver')
-            ->setConstructorArgs(array('root'))
-            ->setMethods(null)
-            ->getMock()
-        ;
+        $resolver = $this->getResolver("root");
 
-        $qb = $this->getMockBuilder('Doctrine\\ORM\\QueryBuilder')
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
+        $result = $resolver->unpack(array('id' => 1));
+        $this->assertEquals(array(
+            'root.id = ?0',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+        ), $result['parameters']);
 
+        $result = $resolver->unpack(array('root.id' => 1));
+        $this->assertEquals(array(
+            'root.id = ?0',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+        ), $result['parameters']);
 
-        $qb->expects($this->exactly(2))->method('getDQLPart')->will($this->returnValue(array()));
-
-        $qb->expects($this->exactly(3))->method('join');
-
-        $this->assertSame($resolver, $resolver->resolveCriteria($qb, array(
-            'test',
-            'test.fooBar'
-        )));
+        $result = $resolver->unpack(array('root.id' => 1, 'root.name' => 'tester'));
+        $this->assertEquals(array(
+            'root.id = ?0',
+            'root.name = ?1',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+            1 => 'tester',
+        ), $result['parameters']);
     }
 
-    public function test_resolve_criteria_adds_where_for_related_property()
+    public function test_unpack_conditions_with_named_parameters()
     {
-        $resolver = $this->getMockBuilder('Ctrl\\RadBundle\\EntityService\\Criteria\\Resolver')
-            ->setConstructorArgs(array('root'))
-            ->setMethods(null)
-            ->getMock()
-        ;
+        $resolver = $this->getResolver("root");
 
-        $qb = $this->getMockBuilder('Doctrine\\ORM\\QueryBuilder')
-            ->disableOriginalConstructor()
-            ->getMock()
-        ;
+        $result = $resolver->unpack(array('id = :test' => 1));
+        $this->assertEquals(array(
+            'root.id = :test',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            'test' => 1,
+        ), $result['parameters']);
 
+        $result = $resolver->unpack(array('id = :test' => array('test' => 1)));
+        $this->assertEquals(array(
+            'root.id = :test',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            'test' => 1,
+        ), $result['parameters']);
 
-        $qb->expects($this->exactly(1))->method('getDQLPart')->will($this->returnValue(array()));
+        $result = $resolver->unpack(array(
+            'id = :test' => 1,
+            'name = :name' => 'tester',
+        ));
+        $this->assertEquals(array(
+            'root.id = :test',
+            'root.name = :name',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            'test' => 1,
+            'name' => 'tester',
+        ), $result['parameters']);
+    }
 
-        $qb->expects($this->exactly(1))->method('join')->with('root.fooBar', 'fooBar');
+    public function test_unpack_conditions_with_mixed_parameters()
+    {
+        $resolver = $this->getResolver("root");
 
-        $qb->expects($this->exactly(1))->method('andWhere')->with('fooBar.id = ?1')->will($this->returnValue($qb));
+        $result = $resolver->unpack(array('id' => 1, 'name = :name' => 'tester'));
+        $this->assertEquals(array(
+            'root.id = ?0',
+            'root.name = :name',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+            'name' => 'tester',
+        ), $result['parameters']);
 
-        $qb->expects($this->exactly(1))->method('setParameter')->with(1, 69)->will($this->returnValue($qb));
+        $result = $resolver->unpack(array('id' => 1, 'name = :name' => 'tester', 'parent' => 2));
+        $this->assertEquals(array(
+            'root.id = ?0',
+            'root.name = :name',
+            'root.parent = ?1',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+            'name' => 'tester',
+            1 => 2,
+        ), $result['parameters']);
+    }
 
-        $this->assertSame($resolver, $resolver->resolveCriteria($qb, array(
-            'fooBar.id' => 69
-        )));
+    public function test_unpack_conditions_with_multiple_parameters_in_single_condition()
+    {
+        $resolver = $this->getResolver("root");
+
+        $result = $resolver->unpack(array('id = :id AND name = :name' => array('id' => 1, 'name' => 'tester')));
+        $this->assertEquals(array(
+            'root.id = :id',
+            'root.name = :name',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            'id' => 1,
+            'name' => 'tester',
+        ), $result['parameters']);
+
+        $result = $resolver->unpack(array('id = :id AND active = true' => array('id' => 1)));
+        $this->assertEquals(array(
+            'root.id = :id',
+            'root.active = true',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            'id' => 1,
+        ), $result['parameters']);
+    }
+
+    public function test_unpack_joins()
+    {
+        $resolver = $this->getResolver("root");
+
+        $result = $resolver->unpack(array('messages'));
+        $this->assertEquals(array(
+            'root.messages' => 'messages',
+        ), $result['joins']);
+        $this->assertEquals(array(), $result['conditions']);
+
+        $result = $resolver->unpack(array('messages.user'));
+        $this->assertEquals(array(
+            'root.messages' => 'messages',
+            'messages.user' => 'user',
+        ), $result['joins']);
+        $this->assertEquals(array(), $result['conditions']);
+
+        $result = $resolver->unpack(array('orders.client.user'));
+        $this->assertEquals(array(
+            'root.orders' => 'orders',
+            'orders.client' => 'client',
+            'client.user' => 'user',
+        ), $result['joins']);
+        $this->assertEquals(array(), $result['conditions']);
+    }
+
+    public function test_unpack_joins_through_conditions()
+    {
+        $resolver = $this->getResolver("root");
+
+        $result = $resolver->unpack(array('messages.id' => 1));
+        $this->assertEquals(array(
+            'root.messages' => 'messages'
+        ), $result['joins']);
+        $this->assertEquals(array(
+            'messages.id = ?0'
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1
+        ), $result['parameters']);
+
+        $result = $resolver->unpack(array('messages.user.id' => 1));
+        $this->assertEquals(array(
+            'root.messages' => 'messages',
+            'messages.user' => 'user',
+        ), $result['joins']);
+        $this->assertEquals(array(
+            'user.id = ?0'
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1
+        ), $result['parameters']);
+
+        $result = $resolver->unpack(array(
+            'messages.user.id' => 1,
+            'orders.client.id' => 2,
+        ));
+        $this->assertEquals(array(
+            'root.messages' => 'messages',
+            'messages.user' => 'user',
+            'root.orders' => 'orders',
+            'orders.client' => 'client',
+        ), $result['joins']);
+        $this->assertEquals(array(
+            'user.id = ?0',
+            'client.id = ?1',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+            1 => 2,
+        ), $result['parameters']);
+
+        $result = $resolver->unpack(array(
+            'messages.user.id' => 1,
+            'orders.client.id = :client' => 2,
+        ));
+        $this->assertEquals(array(
+            'root.messages' => 'messages',
+            'messages.user' => 'user',
+            'root.orders' => 'orders',
+            'orders.client' => 'client',
+        ), $result['joins']);
+        $this->assertEquals(array(
+            'user.id = ?0',
+            'client.id = :client',
+        ), $result['conditions']);
+        $this->assertEquals(array(
+            0 => 1,
+            'client' => 2,
+        ), $result['parameters']);
     }
 }
