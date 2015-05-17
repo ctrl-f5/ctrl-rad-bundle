a1299d5dcdf0b17de771f488e758c68610a7cee7
Nicky De Maeyer <nicky.demaeyer@gmail.com>
2015-05-17 22:38:41 +0200

    code prettify

--- Resources/public/js/google-code-prettify-init.js
+++ Resources/public/js/google-code-prettify-init.js
@@ -1,12 +1,18 @@
-function init() {
-    var prettify = false;
-    var blocks = document.querySelectorAll('pre code')
-    for (var i = 0; i < blocks.length ; i++) {
-        blocks[i].className += 'prettyprint linenums';
-        prettify = true;
-    }
-    if (prettify) {
-        prettyPrint();
+function google_code_prettify()
+{
+    if (prettyPrint) {
+        var prettify = false;
+        var blocks = document.querySelectorAll('pre code');
+        for (var i = 0; i < blocks.length; i++) {
+            if (0 > blocks[i].className.indexOf('prettyprint')) {
+                blocks[i].className += ' prettyprint linenums';
+                prettify = true;
+            }
+        }
+        if (prettify) {
+            prettyPrint();
+        }
     }
 }
-window.onload = init;
+
+window.onload = google_code_prettify();
